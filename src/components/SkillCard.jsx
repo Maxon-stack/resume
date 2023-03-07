ab832c87517b592be5f49da6b39ae65c0ac016ba
Maxon Corvil <corvil.mc@gmail.com>
2023-03-07 17:19:45 -0500

    Build template

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import ProgressiveImage from 'react-progressive-image';
+import LineIcon from "react-lineicons";
 
 const SkillCard = ({ skill }) => {
   console.log(skill)
   return (
     <div className="skill_Card">
-      <header className="skill_card__header">
-        <img className="skill_card__icon" src={skill.icon} alt="HTML5 Logo" />
-      </header>
-      <section className="skill_card__body">
-        <h4 className="skill_card__title">{skill.title}</h4>
-        <span className="skill_card__duration">{`${skill.years} years of Experience`}</span>
-      </section>
+      <div className="face face1">
+        <div className="content">
+          <span className="mi-service-icon">
+            <LineIcon name='react' />
+          </span>
+          <h3>
+            {skill.title}
+          </h3>
+        </div>
+      </div>
+      <div className="face face2">
+        <div className="content">
+          <p>
+            Experience: {skill.years} Years
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
